0ae79c613b6004465e0f9196daf6499e2fe1eea6
catalinabotia <c.botia@uniandes.edu.co>
2020-10-29 15:34:14 +0000

    Prosthetics env

--- prueba.py
+++ prueba.py
@@ -1,9 +1,18 @@
+'''
 from osim.env import L2M2019Env
 
 env = L2M2019Env(visualize=True)
 observation = env.reset()
 for i in range(200):
     observation, reward, done, info = env.step(env.action_space.sample())  #env.action_space.sample() returns a random vector for muscle activations (red active)
+
+'''
+from osim.env import ProstheticsEnv
+env = ProstheticsEnv(visualize=True)
+observation = env.reset()
+#change_model(model='3D', prosthetic=False, difficulty=0,seed=None)
+for i in range(200):
+    o, r, d, i = env.step(env.action_space.sample())
 
 '''construct a controller, i.e. a function from the state space (current positions, velocities and accelerations of joints) to action space (muscle excitations), that will enable to model to travel as far as possible in a fixed amount of time.
 total_reward = 0.0
