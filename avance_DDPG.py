014034d5c976ee0cdde39591c299b08c8d33bd64
ramoshe0 <i.ramosh@outlook.es>
2020-10-31 23:59:13 +0000

    Para ya

--- avance_DDPG.py
+++ avance_DDPG.py
@@ -12,36 +12,30 @@ from chainerrl import replay_buffer
 import numpy as np
 from osim.env import ProstheticsEnv
 from arguments import parser, print_args
-
+args = parser()
 
 env = ProstheticsEnv(visualize=True)
 env.change_model(model='3D', prosthetic=True, difficulty=0, seed=None) #Se empieza con dificultad 0, prótesis y 3D
 #El action space para el modelo escogido es vector de lenght 19
 observation = env.reset(project = True)
 
-#Argumentos
-num_ep = 200
-obs_size = 160#np.asarray(env.observation_space.shape).prod() #158
-print(obs_size)
+# Sizes environment
+#np.asarray(env.observation_space.shape).prod() 
+print(args.obs_size)
 action_size = np.asarray(env.action_space.shape).prod() #19
-hidd_lay = 2
-c_hidd_lay = 30
-actor_lr = 0.0001
-critic_lr = 0.0001
-gamma=0.995 
 
 # Función Q
-q_func = q_functions.FCSAQFunction(obs_size, action_size, n_hidden_channels=hidd_lay, n_hidden_layers=c_hidd_lay)
+q_func = q_functions.FCSAQFunction(args.obs_size, action_size, n_hidden_channels=args.hidd_lay, n_hidden_layers=args.c_hidd_lay)
 
 # Policy 
-pi = policy.FCDeterministicPolicy(obs_size, action_size=action_size, n_hidden_channels=hidd_lay, n_hidden_layers=c_hidd_lay, min_action=env.action_space.low, max_action=env.action_space.high, bound_action=True)
+pi = policy.FCDeterministicPolicy(args.obs_size, action_size=action_size, n_hidden_channels=args.hidd_lay, n_hidden_layers=args.c_hidd_lay, min_action=env.action_space.low, max_action=env.action_space.high, bound_action=True)
 
 
 # El Modelo
 
 model = DDPGModel(q_func=q_func, policy=pi)
-opt_actor = optimizers.Adam(alpha=actor_lr)
-opt_critic = optimizers.Adam(alpha=critic_lr)
+opt_actor = optimizers.Adam(alpha=args.actor_lr)
+opt_critic = optimizers.Adam(alpha=args.critic_lr)
 opt_actor.setup(model['policy'])
 opt_critic.setup(model['q_function'])
 opt_actor.add_hook(chainer.optimizer.GradientClipping(1.0), 'hook_a')
@@ -50,17 +44,12 @@ rbuf = replay_buffer.ReplayBuffer(5 * 10 ** 5 )
 ou_sigma = (env.action_space.high - env.action_space.low) * 0.2
 explorer = explorers.AdditiveOU(sigma=ou_sigma)
 
-def phi(obs):
-    """ Convert the data type of the observation to float-32
-    Input: observation (obs)
-    Output:  the processed observation 
-    """ 
-    obs=np.array(obs)
-    return obs.astype(np.float32)
+
+phi = lambda x: np.array(x).astype(np.float32, copy=False) #COnverir datatype de la observación 
 
 
 # Agent
-agent = DDPG(model, opt_actor, opt_critic, rbuf, gamma=gamma,
+agent = DDPG(model, opt_actor, opt_critic, rbuf, gamma=args.gamma,
                  explorer=explorer, replay_start_size=5000,
                  target_update_method='soft',
                  target_update_interval=1,
@@ -73,7 +62,7 @@ agent = DDPG(model, opt_actor, opt_critic, rbuf, gamma=gamma,
 
 # construct a controller, i.e. a function from the state space (current positions, velocities and accelerations of joints) to action space (muscle excitations), that will enable to model to travel as far as possible in a fixed amount of time.
 total_reward = 0.0
-for ep in range(1, num_ep+ 1):
+for ep in range(1, args.numep + 1):
     # make a step given by the controller and record the state and the reward
     obs = env.reset()
     reward = 0
@@ -88,3 +77,8 @@ for ep in range(1, num_ep+ 1):
 
 # Your reward is
 print("Total reward %f" % total_reward)
+
+
+#Sources
+# https://github.com/montaserFath/Reinforcement-Learning-for-Prosthetics/blob/master/RL/DDPG/DDPG_Prosthetic.ipynb
+# https://github.com/chainer/chainerrl/blob/master/examples/quickstart/quickstart.ipynb
